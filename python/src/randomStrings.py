1fd327e928c6b102343a165e36e3aeab0226be30
seagull1089 <aravind.arjun@gmail.com>
2012-07-05 13:36:26 -0700

    minor tweaks

--- python/src/randomStrings.py
+++ python/src/randomStrings.py
@@ -8,6 +8,7 @@ method to generate random strings from ascii characters and digits.
 def generateRandomString(length = 32 , chars=string.ascii_uppercase + string.digits):
 	return ''.join(random.choice(chars) for x in range(length))
 
-for i in range(0,100):
-	print generateRandomString(100)
+if __name__=="__main__":
+	for i in xrange(0,100):
+		print generateRandomString(100)
 	
